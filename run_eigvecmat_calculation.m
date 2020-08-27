16856783d1ba1dd7b6ca24a2014017760295b3b4
Junha Shin <junha.shin@wisc.edu>
2020-08-27 00:41:36 -0500

    Update run_eigvecmat_calculation.m
    simplified by removing gene list input.

--- run_eigvecmat_calculation.m
+++ run_eigvecmat_calculation.m
@@ -2,10 +2,9 @@ function run_eigvecmat_calculation(k)
 
 for species={'ath','mtr','osa','ppa','stu','zma'}
 	matrix=sprintf('sample_data/%s_sample_matrix.txt',species{1})
-	gnlist=sprintf('sample_data/%s_matrix_gnids.txt',species{1})
 	outpref=species{1};
 
-	eigvecmat_calc(matrix,gnlist,k,outpref)
+	eigvecmat_calc(matrix,k,outpref)
 end
 
 quit
