3e1d426a7fa7870ebf07fb013b97af52a21678e2
Junha Shin <junha.shin@wisc.edu>
2020-08-27 02:17:48 -0500

    Update run_muscari.sh
    Open parameter arguments p and x.

--- run_muscari.sh
+++ run_muscari.sh
@@ -2,11 +2,11 @@
 set -u
 
 k=$1		# number of clusters
-outdir=$2	# name of output directory
+p=$2		# transition probability (starting default=0.8)
+x=$3		# fixed covariance (starting default=0.1)
+best=$4		# best known species name (e.g. ath)
+outdir=$5	# name of output directory
 
-best=ath	# best known species name
-p=0.8		# transition probability (default=0.8)
-x=0.01		# fixed covariance (default=0.01)
 orderf=sample_data/SpeciesOrder.txt	# species order filename
 ogidsf=sample_data/sample_OGID.txt	# OGID filename
 treef=sample_data/SpeciesTree.txt	# species tree filename
